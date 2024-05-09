1ba6df862ef27421fee11ee7f00d447b124fcefe
abol kian <ashkan1saleh1n@gmail.com>
2024-05-09 13:12:09 +0330

    creating admin register

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Genre, Book, BookInstace, Author, Language
+
+
+class BookInlineAdmin(admin.StackedInline):
+    model = Book
+    extra = 0
+    list_display = ['title', 'author', 'display_genre']
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookInlineAdmin]
+
+    list_display = ['last_name', 'first_name', 'date_of_birth', 'date_of_death']
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+# @admin.register(BookInstace)
+class BookInstaceAdmin(admin.TabularInline):
+    model = BookInstace
+    list_display = ['book', 'status', 'due_back', 'id']
+    list_filter = ['status', 'due_back']
+    extra = 0
+
+    fieldsets = (
+        (None, {'fields': ('book', 'imprint', 'id')}),
+        ('Availability', {'fields': ('status', 'due_back')})
+    )
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    inlines = [BookInstaceAdmin]
+
+    list_display = ['title', 'author', 'display_genre']
+
 
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Author, AuthorAdmin)
+
+
