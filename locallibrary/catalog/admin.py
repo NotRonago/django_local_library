4e9eba391a55c25deaed7f31ce4c1df6f11a6a4b
ashkan salehnejad <you@example.comashkan1saleh1n@gmail.com>
2024-05-12 07:56:30 +0330

    Making generic views

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -27,6 +27,15 @@ class BookInstaceAdmin(admin.TabularInline):
         ('Availability', {'fields': ('status', 'due_back')})
     )
 
+@admin.register(BookInstace)
+class BookIAdmin(admin.ModelAdmin):
+    list_display = ['book', 'status', 'due_back', 'id']
+    list_filter = ['status', 'due_back']
+
+    fieldsets = (
+        (None, {'fields': ('book', 'imprint', 'id')}),
+        ('Availability', {'fields': ('status', 'due_back')})
+    )
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
